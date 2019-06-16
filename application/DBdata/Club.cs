be9e9a2fe8727c62c862ba3f9b9799bd6ae7f78c
Krauzi <veris11292@gmail.com>
2019-06-16 18:57:00 +0200

    added player and club class

---
+++ application/DBdata/Club.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace application.DBdata
+{
+    class Club
+    {
+        public int? Id { get; set; } // zabezpieczenie przed nullem
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public User() { }
+
+        public User(IDataReader dataReader)
+        {
+            // dataReader.Read()
+            Id = (int)dataReader["id_u"];
+            FirstName = (string)dataReader["fname"];
+            LastName = (string)dataReader["lname"];
+            BirthDate = (DateTime)dataReader["bdate"];
+        }
+
+        public override string ToString()
+        {
+            // return string.Format("ID: {0}, First name: {1}, Last name: {2}, Birth Date: {3}.", Id, FirstName, LastName, BirthDate);
+            return $"{Id} {FirstName} {LastName} {BirthDate.Date.Year}";
+        }
+    }
+}
---
+++ application/DBdata/Player.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace application.DBdata
+{
+    class Player
+    {
+        public int? Id { get; set; } // zabezpieczenie przed nullem
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Position { get; set; }
+        public int Height { get; set; } 
+        public int Weight { get; set; }
+
+        private int number;
+
+        public Player() { }
+
+        public Player(IDataReader dataReader)
+        {
+            // dataReader.Read()
+            Id = (int)dataReader["id_u"];
+            FirstName = (string)dataReader["name"];
+            LastName = (string)dataReader["lastname"];
+            BirthDate = (DateTime)dataReader["dateofbirth"];
+            Position = (string)dataReader["position"];
+
+            if (dataReader["height"] == null) Height = 0;
+            else Height = (int)dataReader["height"];
+
+            if (dataReader["weight"] == null) Weight = 0;
+            else Weight = (int)dataReader["height"];
+        }
+
+        public Tuple<int, string, string, DateTime, int, int> GetAnswer()
+        {
+            return new Tuple<int, string, string, DateTime, int, int>((int)Id, FirstName, LastName, BirthDate, Height, Weight);
+        }
+    }
+}
